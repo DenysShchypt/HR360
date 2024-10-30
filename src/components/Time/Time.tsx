2319a428042de93d94ff7704f82c2b7d742fa6e3
DenysShchypt <denysshchyptsovit@gmail.com>
2024-10-30 21:36:44 +0200

    add interval time

--- src/components/Time/Time.tsx
+++ src/components/Time/Time.tsx
@@ -1,12 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { datePart, timePart } from '../../utils/helpers/time';
 import styles from './Time.module.css';
 
 const Time: FC = () => {
+  const [time, setTime] = useState(timePart);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(timePart);
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className={styles.time_wrap}>
       <p className={styles.time}>{datePart}</p>
-      <p className={styles.time}>{timePart}</p>
+      <p className={styles.time}>{time}</p>
     </div>
   );
 };
